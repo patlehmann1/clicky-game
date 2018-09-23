1ab8fcd06c253359b759111d8441353bbf7f9b2c
Patrick Lehmann <patlehmann1@gmail.com>
2018-09-23 12:21:32 -0400

    total to top

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -3,7 +3,7 @@ import './ProgressBar.css';
 
 const ProgressBar = props => (
 	<nav className = "container-fluid">
-		<p>Score: {props.score} </p><p>Total Score: {props.topScore}</p>
+		<p>Score: {props.score} </p><p>Top Score: {props.topScore}</p>
 	</nav>
 );
 export default ProgressBar;
